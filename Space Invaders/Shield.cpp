c1975dacac01afbcf908981ba165bdcb8a055df8
lululul lololol <canossifrancesco4@protonmail.com>
2024-02-06 14:19:51 +0100

    each 'blank' pixelColor is now correctly associated with its corresponding pixelRec

--- Space Invaders/Shield.cpp
+++ Space Invaders/Shield.cpp
@@ -22,43 +22,51 @@ void Shield::Draw() {
 			if (row == 0 && (column == 0 || column == 1 || column == 2 || column == 3 
 				|| column == MAX_WIDTH / PIXEL_SIZE - 4 || column == MAX_WIDTH / PIXEL_SIZE - 3
 				|| column == MAX_WIDTH / PIXEL_SIZE - 2 || column == MAX_WIDTH / PIXEL_SIZE - 1)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			//second row 'deleted' pixels
 			else if (row == 1 && (column == 0 || column == 1 || column == 2
 				|| column == MAX_WIDTH / PIXEL_SIZE - 3
 				|| column == MAX_WIDTH / PIXEL_SIZE - 2 || column == MAX_WIDTH / PIXEL_SIZE - 1)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			//third row 'deleted' pixels
 			else if (row == 2 && (column == 0 || column == 1
 				|| column == MAX_WIDTH / PIXEL_SIZE - 2 || column == MAX_WIDTH / PIXEL_SIZE - 1)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			//fourth row 'deleted' pixels
 			else if (row == 3 && (column == 0
 				|| column == MAX_WIDTH / PIXEL_SIZE - 1)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			//last row and 2nd to last row 'deleted' pixels
 			else if ((row == MAX_HEIGHT / PIXEL_SIZE - 1 || row == MAX_HEIGHT / PIXEL_SIZE - 2) 
 				&& (column >= 4 && column <= MAX_WIDTH / PIXEL_SIZE - 5)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			//3rd to last row 'deleted' pixels
 			else if ((row == MAX_HEIGHT / PIXEL_SIZE - 3 || row == MAX_HEIGHT / PIXEL_SIZE - 4)
 				&& (column >= 5 && column <= MAX_WIDTH / PIXEL_SIZE - 6)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			//next 2 rows
 			else if ((row == MAX_HEIGHT / PIXEL_SIZE - 5 || row == MAX_HEIGHT / PIXEL_SIZE - 6)
 				&& (column >= 6 && column <= MAX_WIDTH / PIXEL_SIZE - 7)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			//next 2 rows
 			else if ((row == MAX_HEIGHT / PIXEL_SIZE - 7 || row == MAX_HEIGHT / PIXEL_SIZE - 8)
 				&& (column >= 7 && column <= MAX_WIDTH / PIXEL_SIZE - 8)) {
-				DrawRectangleRec(pixelRec[row][column], BLANK);
+				pixelColor[row][column] = BLANK;
+				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
 			}
 			else {
 				DrawRectangleRec(pixelRec[row][column], pixelColor[row][column]);
